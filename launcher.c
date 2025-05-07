5c192af74ea7073aafe23489a985a9baef34f613
Pranav G N <152719365+pranavgn12@users.noreply.github.com>
2025-05-08 00:30:18 +0530

    Update launcher.c

--- launcher.c
+++ launcher.c
@@ -4,5 +4,5 @@
 void main() {
     HWND hwnd = GetConsoleWindow();
     ShowWindow(hwnd, SW_HIDE);
-	system("conhost sudoku.exe");
+	system("conhost sudoku.exe");  //conhost command brings up legcay or old cmd that existed before newer windows terminal.
 }
